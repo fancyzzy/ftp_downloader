37ffeb7bb616477518383ebb63c1f5c1ab1e3bcf
Felix Zhang <felix.zhang@nokia-sbell.com>
2017-10-01 10:00:42 +0800

    Fix circlular import issue
    Create a new module 'log_reserve.py' which can be imported by others

--- read_olook.py
+++ read_olook.py
@@ -8,7 +8,8 @@ import threading
 MAPI = Dispatch("Outlook.Application").GetNamespace("MAPI")
 #print "DEBUG 6= ",MAPI.GetDefaultFolder(6)
 import pythoncom
-from my_ftp import printl
+from log_reserve import printl
+
 #New mails are to be checked after this time point
 TIME_POINT = time.strftime('%m/%d/%y %H:%M:%S',time.localtime(time.time()))
 #print("type TIME_POINT=",type(TIME_POINT))
